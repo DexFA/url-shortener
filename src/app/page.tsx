8f03ae29192664c3eb3b98dcf2184ac7985a15ba
Fidelis Adaikwu <110574901+DexFA@users.noreply.github.com>
2023-08-29 20:00:51 +0100

    Import Poppins google font for blobal usage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import Header from "../components/header/header";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+	weight: ["500", "700"],
+	subsets: ["latin"],
+});
 
 export default function Home() {
 	return (
-		<div>
+		<div className={poppins.className}>
 			<Header />
 			<main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
 		</div>
