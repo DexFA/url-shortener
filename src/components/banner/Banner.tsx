63d3a00bb55c002388fe3425fed55aa50edce1a1
Fidelis Adaikwu <110574901+DexFA@users.noreply.github.com>
2023-08-31 01:06:29 +0100

    Edit banner component.

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -5,11 +5,11 @@ import HeroImage from "../../../public/illustration-working.svg";
 
 const Banner = () => {
 	return (
-		<div>
+		<div className="flex">
 			<div className="flex max-w-screen-xl h-[70%] m-auto py-24 items-center">
 				<div className="grid max-w-xl">
 					<h1 className="font-[700] text-7xl">More than just shorten links</h1>
-					<p className="max-w-md mt-4">
+					<p className="max-w-lg mt-4 text-xl">
 						Build your brand&apos;s recognitino and get detailed insights on how
 						your links are performing.
 					</p>
@@ -18,7 +18,9 @@ const Banner = () => {
 					</div>
 				</div>
 			</div>
-			<Image src={HeroImage} alt="Picture of the author" />
+			<div className="flex overflow-hidden">
+				<Image src={HeroImage} alt="Picture of the author" />
+			</div>
 		</div>
 	);
 };
